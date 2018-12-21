e7056d8c7a21cae951e037e83bc4297553998b18
JoePark1993 <32422763+JoePark1993@users.noreply.github.com>
2018-12-20 20:39:55 -0800

    !help command

--- puddle-bot.py
+++ puddle-bot.py
@@ -1,9 +1,12 @@
 import discord
 import asyncio
 import markovify
+import traceback
+import os
 from constants import TOKEN
 
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
@@ -12,40 +15,52 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+
 @client.event
 async def on_message(message):
-    if message.content.startswith('!logs'):
+    if message.content.startswith('!getmsgs'):
         user_list = {}
         channels = message.server.channels
         for channel in channels:
             print("Working on " + channel.name)
             if channel.name == "isekaid":
                 continue
-            if channel.name =="test2":
+            if channel.name == "test2":
                 continue
-            async for message in client.logs_from(channel, limit=10000):
+            async for message in client.logs_from(channel, limit=11000):
                 if message.author in user_list:
-                    user_list[message.author] += (message.content + "\n")
+                    user_list[message.author].append(message.content)
                 else:
-                    user_list[message.author] = (message.content + "\n")
-        print("Done")
+                    user_list[message.author] = [message.content]
+        print("Done getting messages")
         for user in user_list:
-            with open(user.name + ".txt", "w") as file:
-                file.write(user_list[user])
+            print("Writing file for " + user.name)
+            try:
+                with open(user.name + user.id + ".txt", "w") as file:
+                    for msg in user_list[user]:
+                        file.write("%s\n" % msg)
+            except IOError:
+                print(traceback.format_exc())
+        print("Done!")
 
     if message.content.startswith('!talk'):
         if message.mentions:
             filename = message.mentions[0].name
+            user_id = message.mentions[0].id
             try:
-                with open(filename + ".txt") as file:
+                with open(filename + user_id + ".txt", "r") as file:
                     text = file.read()
-                text_model = markovify.NewlineText(text)
-                try:
-                    for i in range(5):
-                        await client.send_message(message.channel, text_model.make_sentence(tries=100), tts=False)
-                except discord.HTTPException:
-                    await client.send_message(message.channel, "Generated null message.")
+                    text_model = markovify.NewlineText(text)
+                    try:
+                        for i in range(5):
+                            await client.send_message(message.channel, text_model.make_sentence(tries=100))
+                    except discord.HTTPException:
+                        await client.send_message(message.channel, "Generated null message.")
             except IOError:
                 print("File not found")
 
+    if message.content.startswith('!help'):
+        client.send_message("List of commands for Puddle-bot!")
+        client.send_message("!getmsgs - Record all user messages in server")
+        client.send_message("!talk <@mention-user> <# of messages. Default = 5> -  ")
 client.run(TOKEN)
